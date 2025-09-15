857d9ce46630b790f519cf172413a32bc584251a
lesTraigoAmor <zebastiansanchez@gmail.com>
2025-09-15 17:14:17 -0300

    cambios en navbar, ubicacion de textos e imagenes

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const menuToggle = document.querySelector('.menu-toggle');
-  const menu = document.querySelector('.menu');
-
-  menuToggle.addEventListener('click', () => {
-    menu.classList.toggle('active');
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.menu');
+  
+    menuToggle.addEventListener('click', () => {
+      menu.classList.toggle('active');
+    });
   });
-});
+
